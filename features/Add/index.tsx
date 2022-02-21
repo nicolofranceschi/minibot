719dd53ab77569e80b1bca732588083bd3292d36
Giovanni Incammicia <giovanni.incammicia@gmail.com>
2022-02-21 17:06:15 +0100

    Add Form

--- features/Add/index.tsx
+++ features/Add/index.tsx
@@ -1,66 +1,90 @@
 import { useTheme } from "@emotion/react";
-import { Box, Paper, TextField, Typography, Autocomplete, Button } from "@mui/material";
+import { Box, TextField, Typography, Autocomplete, Button, Checkbox } from "@mui/material";
 import { useForm,Controller } from "react-hook-form";
 
 interface FormAdd {
     codiceArticolo: string;
     description: string;
     tipoFinitura: string;
+    rigato: boolean;
 }
 
 const top100Films = [
-    { label: 'The Shawshank Redemption', year: 1994 },
-    { label: 'The Godfather', year: 1972 },
-    { label: 'The Godfather: Part II', year: 1974 },
-    { label: 'The Dark Knight', year: 2008 },
-    { label: '12 Angry Men', year: 1957 },
-    { label: "Schindler's List", year: 1993 },
-    { label: 'Pulp Fiction', year: 1994 }
+    'test',
+    'abc',
+    'def'
 ]
 
+const calcStateColor = (isError, theme) => {
+    if (isError) return theme.palette.error.main
+    return theme.palette.success.main
+}
+
 export default function Add() {
+    const { register, handleSubmit,control, formState: { errors } } = useForm<FormAdd>({
+        mode: 'onChange',
+        criteriaMode: 'all'
+    });
 
-    const { register, handleSubmit,control, watch, formState: { errors } } = useForm<FormAdd>();
-
-    const theme = useTheme() as any;
-
+ 
     const onSubmit = (data: FormAdd) => {
         console.log(data);
     }
-
-    console.log(theme)
-
     return (
         <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
             <Box sx={{ padding: '1rem', display: 'flex', flexDirection: "column", gap: 1 }}>
-                <Paper elevation={4} sx={{ padding: '1rem', display: 'flex ', gap: 2, flexDirection: "column", borderRadius: '1rem' , border:`2px solid ${theme.palette.warning.main}` }}>
-                    <Typography variant="button">Codice articolo</Typography>
-                    <TextField id="id" variant="outlined" placeholder="Campo alfanumerico" {...register("codiceArticolo")} />
-                </Paper>
-                <Paper elevation={4} sx={{ padding: '1rem', display: 'flex ', gap: 2, flexDirection: "column", borderRadius: '1rem',border:`2px solid ${theme.palette.success.main}` }}>
-                    <Typography variant="button">note</Typography>
+                <ErrorBox label="Codice articolo" error={errors.codiceArticolo?.message}>
+                    <TextField id="id" variant="outlined" placeholder="Campo alfanumerico" {...register("codiceArticolo", {
+                        required: 'Il campo è obbligatorio',
+                        minLength: {value: 10, message: 'Il campo deve contenere almeno 10 caratteri'},
+                    })} />
+                </ErrorBox>
+                <ErrorBox label="Note" error={errors.description?.message}>
                     <TextField multiline rows={6} id="description" variant="outlined" placeholder="Campo alfanumerico" {...register("description")} />
-                </Paper>
-                <Paper elevation={4} sx={{ padding: '1rem', display: 'flex ', gap: 2, flexDirection: "column", borderRadius: '1rem',border:`2px solid ${theme.palette.error.main}` }}>
-                    <Typography variant="button">Tipo Finitura</Typography>
+                </ErrorBox>
+                <ErrorBox label="Tipo Finitura" error={errors.tipoFinitura?.message}>
                     <Controller
                         name="tipoFinitura"
                         control={control}
                         rules={{ required: true }}
-                        render={({ field }) => ( 
-                        <Autocomplete
-                            {...field}
-                            disablePortal
-                            id="tipoFinitura"
-                            options={top100Films}
-                            sx={{ width: 300 }}
-                            renderInput={(params) => <TextField {...params} label="Seleziona tipo finitura" />}
-                        />
+                        render={({ field }) => (
+                            <Autocomplete
+                                value={field.value ?? null}
+                                onChange={(_, value) => field.onChange({ target: { value } })}
+                                disablePortal
+                                options={top100Films}
+                                sx={{ width: 300 }}
+                                renderInput={(params) => <TextField {...params} label="Seleziona tipo finitura" />} />
                         )}
                     />
-                </Paper>
+                </ErrorBox>
+                <ErrorBox label="Checkbox">
+                    <Controller
+                        name="rigato"
+                        control={control}
+                        render={({ field }) => (
+                            <Checkbox
+                                checked={field.value ?? false}
+                                onChange={(_, value) => field.onChange({ target: { value } })} />
+                        )}
+                    />
+                </ErrorBox>
+                
                 <Button variant="contained" type="submit" sx={{padding: '1rem', display: 'flex' , borderRadius: '1rem'}}>SALVA</Button>
             </Box>
         </form>
     )
+}
+
+function ErrorBox ({ children, label, error }: ChildrenProps & { label: string; error?: string }) {
+    const theme = useTheme();
+
+    const color = calcStateColor(error, theme);
+    return (
+        <Box sx={{ padding: '1rem', display: 'flex ', gap: 2, flexDirection: "column", borderRadius: '1rem', outline: `2px solid ${color}`, outlineOffset: '-2px' }}>
+            <Typography variant="button">{label}</Typography>
+            {children}
+            {error && <Typography variant="caption" color="error">{error}</Typography>}
+        </Box>
+    );
 }
